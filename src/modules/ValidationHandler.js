d76e35ed74a82c9ceb2ca105496559ae6b36e96d
Lee Rogers <lrogersgm@gmail.com>
2021-06-01 14:33:07 +0100

    Prettier reconfig

--- src/modules/ValidationHandler.js
+++ src/modules/ValidationHandler.js
@@ -1,68 +1,68 @@
-import { UI } from './UI'
+import { UI } from './UI';
 
 export const ValidationHandler = (() => {
     const _determineError = (input, typeString) => {
         if (input.validity.valueMissing) {
             if (typeString === 'email address') {
-                return `Please enter an ${typeString}.`
+                return `Please enter an ${typeString}.`;
             } else {
-                return `Please enter a ${typeString}.`
+                return `Please enter a ${typeString}.`;
             }
         } else if (input.validity.typeMismatch) {
-            return `Please enter a valid ${typeString}.`
+            return `Please enter a valid ${typeString}.`;
         } else if (input.validity.patternMismatch) {
-            return `Please enter a valid ${typeString}.`
+            return `Please enter a valid ${typeString}.`;
         } else if (input.validity.tooShort) {
-            return `Your ${typeString} should be at least ${input.minLength} characters long.`
+            return `Your ${typeString} should be at least ${input.minLength} characters long.`;
         }
-    }
+    };
 
     const emailHandle = (emailInput) => {
         if (emailInput.validity.valid) {
-            return true
+            return true;
         } else {
-            return _determineError(emailInput, 'email address')
+            return _determineError(emailInput, 'email address');
         }
-    }
+    };
 
     const countryHandle = (countryInput) => {
         if (countryInput.validity.valid) {
-            return true
+            return true;
         } else {
-            return _determineError(countryInput, 'country')
+            return _determineError(countryInput, 'country');
         }
-    }
+    };
 
     const postcodeHandle = (postcodeInput) => {
         if (postcodeInput.validity.valid) {
-            return true
+            return true;
         } else {
-            return _determineError(postcodeInput, 'postcode')
+            return _determineError(postcodeInput, 'postcode');
         }
-    }
+    };
 
     const passwordHandle = (passwordInput) => {
         if (passwordInput.validity.valid) {
-            return true
+            return true;
         } else {
-            return _determineError(passwordInput, 'password')
+            return _determineError(passwordInput, 'password');
         }
-    }
+    };
 
     const passwordConfirmHandle = (passwordConfirmInput, passwordInput) => {
         if (
             passwordConfirmInput.validity.valid &&
             passwordConfirmInput.value === passwordInput.value
         ) {
-            return true
+            return true;
         } else {
             if (passwordConfirmInput.value !== passwordInput.value) {
-                return 'Password and password confirmation do not match.'
+                return 'Password and password confirmation do not match.';
             } else {
-                return _determineError(passwordConfirmInput, 'password')
+                return _determineError(passwordConfirmInput, 'password');
             }
         }
-    }
+    };
 
     const submitHandle = (
         event,
@@ -78,23 +78,23 @@ export const ValidationHandler = (() => {
         passwordConfirmError
     ) => {
         if (emailHandle(emailInput) !== true) {
-            UI.displayError(emailError, emailHandle(emailInput))
-            event.preventDefault()
+            UI.displayError(emailError, emailHandle(emailInput));
+            event.preventDefault();
         }
 
         if (countryHandle(countryInput) !== true) {
-            UI.displayError(countryError, countryHandle(countryInput))
-            event.preventDefault()
+            UI.displayError(countryError, countryHandle(countryInput));
+            event.preventDefault();
         }
 
         if (postcodeHandle(postcodeInput) !== true) {
-            UI.displayError(postcodeError, postcodeHandle(postcodeInput))
-            event.preventDefault()
+            UI.displayError(postcodeError, postcodeHandle(postcodeInput));
+            event.preventDefault();
         }
 
         if (passwordHandle(passwordInput) !== true) {
-            UI.displayError(passwordError, passwordHandle(passwordInput))
-            event.preventDefault()
+            UI.displayError(passwordError, passwordHandle(passwordInput));
+            event.preventDefault();
         }
 
         if (
@@ -103,10 +103,10 @@ export const ValidationHandler = (() => {
             UI.displayError(
                 passwordConfirmError,
                 passwordConfirmHandle(passwordConfirmInput, passwordInput)
-            )
-            event.preventDefault()
+            );
+            event.preventDefault();
         }
-    }
+    };
 
     return {
         emailHandle,
@@ -115,5 +115,5 @@ export const ValidationHandler = (() => {
         passwordHandle,
         passwordConfirmHandle,
         submitHandle,
-    }
-})()
+    };
+})();
